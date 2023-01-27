a93f2ff8cac6500128f43fb85f1c4e6e5e6257e9
KipronohVincent <kipronohvincent43@gmail.com>
2023-01-27 09:42:22 +0300

    Basic application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <header className="App-header">
+                <img src={logo} alt="logo"></img>
+                <h1>School dashboard</h1>
+            </header>
+            <div className="linea"></div>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+            </div>
+            <div className="linea"></div>
+            <footer className="App-footer">
+                <p>Copyright 2020 - holberton School</p>
+            </footer>
+        </div>
+    );
 }
-
-export default App;
+  
